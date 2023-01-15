32b0e4947af946286afc053ea8518641761b22a5
Sagar <sagarfeb298@gmail.com>
2023-01-15 11:16:16 +0530

    created the code for translator

--- off_translator.py
+++ off_translator.py
@@ -1,8 +1,38 @@
 from translate import Translator
 
 s = input("entre the sentence you want to translate: ")
-translator= Translator(to_lang="zh")
+m = ["hi","ja","es","zh"]
+while True:
+    t = input("entre the code in which language you want to translate:\n1.Hindi-hi\n2.Japanese-ja\n3.spanish-es\n4.Chinese-zh\n")
+    if t in m:
+        translator= Translator(to_lang=t)
+        break
+    else:
+        n = input("You putted the wrong code\nChoose 1 to put the code again\nPut anything to get it translated randomly\n")
+        if n=="1":
+            continue
+        else:
+            translator = Translator(to_lang="hi")
+            break
 
-try:
-    with open("/my_f.txt",mode="r") as my_file:
-        text = 
+
+while True:
+
+    try:
+        with open("my_f.txt",mode="r") as my_file:
+            text = my_file.read()
+            if text == s:
+                translation = translator.translate(text)
+                print(translation)
+            else:
+                raise FileNotFoundError
+    except FileNotFoundError:
+        with open("my_f.txt", mode="w") as my_file:
+            if text:
+                my_file.truncate(0)
+                my_file.write(s)
+            else:
+                my_file.write(s)
+    else:
+        break
+
